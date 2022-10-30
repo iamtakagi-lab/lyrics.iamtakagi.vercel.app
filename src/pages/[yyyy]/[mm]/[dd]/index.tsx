67326dc5e22ca41055a41d9143a5e61c53c41666
takagi <iam.takagi.dev@gmail.com>
2022-10-30 15:09:29 +0900

    feat: Cache-Control

--- src/pages/[yyyy]/[mm]/[dd]/index.tsx
+++ src/pages/[yyyy]/[mm]/[dd]/index.tsx
@@ -20,6 +20,8 @@ type Props = {
 };
 
 export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
+  ctx.res.setHeader("Cache-Control", "public, max-age=31536000, immutable")
+
   const { yyyy, mm, dd } = ctx.query;
   const date = `${yyyy}/${mm}/${dd}`;
 
