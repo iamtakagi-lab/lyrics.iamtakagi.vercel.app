d87dc495817e6ae4068579669cb380db074ab562
takagi <iam.takagi.dev@gmail.com>
2022-10-30 11:44:18 +0900

    feat: error page

--- src/pages/[yyyy]/[mm]/[dd]/index.tsx
+++ src/pages/[yyyy]/[mm]/[dd]/index.tsx
@@ -4,15 +4,17 @@ import { database } from "../../../../common/supabase";
 import Head from "next/head";
 import "moment/locale/ja";
 import moment from "moment";
+import { STATUS_CODES } from "http";
 
 type Props = {
-  date: string;
-  song: Song;
-  lastfmUserId: string;
-  siteDomain: string;
-  twitterId: string;
+  date?: string;
+  song?: Song;
+  lastfmUserId?: string;
+  siteDomain?: string;
+  twitterId?: string;
   error?: {
-    status: number;
+    statusCode: number;
+    statusLabel: string;
     message: string;
   };
 };
@@ -20,11 +22,23 @@ type Props = {
 export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
   const { yyyy, mm, dd } = ctx.query;
   const date = `${yyyy}/${mm}/${dd}`;
-  console.log(date);
 
-  /**
-   * TODO: date の正規表現チェック
-   */
+  // それは本当に日付ですか
+  if (isNaN(new Date(date).getDate())) {
+    ctx.res.statusCode = 500
+    return {
+      props: {
+        siteDomain: process.env.SITE_DOMAIN ?? "",
+        lastfmUserId: process.env.LASTFM_USER_ID ?? "",
+        twitterId: process.env.TWITTER_ID ?? "",
+        error: {
+          statusCode: 500,
+          statusLabel: STATUS_CODES[500],
+          message: "📅 正しい日付を指定してください",
+        },
+      },
+    };
+  }
 
   const { data, error, status } = await database
     .from("songs")
@@ -32,12 +46,24 @@ export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
     .limit(1)
     .eq("date", date);
   if (!data || !data.length || !data[0] || error) {
+    ctx.res.statusCode = 404
     return {
-      notFound: true,
+      props: {
+        date,
+        siteDomain: process.env.SITE_DOMAIN ?? "",
+        lastfmUserId: process.env.LASTFM_USER_ID ?? "",
+        twitterId: process.env.TWITTER_ID ?? "",
+        error: {
+          statusCode: 404,
+          statusLabel: STATUS_CODES[404],
+          message: `📅 ${date} の歌詞はありません`,
+        },
+      },
     };
   }
 
   //console.log(data[0]);
+  ctx.res.statusCode = 200
   return {
     props: {
       date,
@@ -49,14 +75,71 @@ export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
   };
 };
 
-export const DatePage = ({ date, lastfmUserId, song, siteDomain, twitterId }: Props) => {
+export const DatePage = ({
+  date,
+  lastfmUserId,
+  song,
+  siteDomain,
+  twitterId,
+  error,
+}: Props) => {
   const yesterday = moment(date).add(-1, "days").format("YYYY/MM/DD");
   const tomorrow = moment(date).add(1, "days").format("YYYY/MM/DD");
 
-  return (
-    song && (
+  if (error && siteDomain && twitterId) {
+    return (
       <>
-        <Seo title={`${date}: ${song.name} ― ${song.artist}`} description={`${date} の曲は ${song.artist} の ${song.name} でした`} ogp={song.imageUrl} siteDomain={siteDomain} twitterId={twitterId} />
+        <Seo
+          title={`${error.statusCode}: ${error.message}`}
+          description={`${error.message}`}
+          ogp={'https://i.imgur.com/l1M6ZDy.jpg'}
+          siteDomain={siteDomain}
+          twitterId={twitterId}
+        />
+        <div className="main">
+          <div className="content">
+            <h1>
+              {error.statusCode}: {error.statusLabel}
+            </h1>
+            <h2>{error.message}</h2>
+
+            {date && (
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  marginTop: "1rem",
+                }}
+              >
+                <a href={`/${yesterday}`}>←{yesterday}</a>
+                <a href={`/${tomorrow}`}>{tomorrow}→</a>
+              </div>
+            )}
+
+            <div style={{ marginTop: "1rem" }}>
+              <a href={`https://www.last.fm/user/${lastfmUserId}`}>
+                🎧 last.fm/@{lastfmUserId}
+              </a>{" "}
+              の直近トップトラックから、1日1回ランダムに歌詞をピックアップしています
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
+
+  return (
+    song &&
+    siteDomain &&
+    twitterId && (
+      <>
+        <Seo
+          title={`${date}: ${song.name} ― ${song.artist}`}
+          description={`${date} の曲は ${song.artist} の ${song.name} でした`}
+          ogp={song.imageUrl}
+          siteDomain={siteDomain}
+          twitterId={twitterId}
+        />
         <div className="main">
           <div className="content">
             <h1>📅 {date}</h1>
@@ -78,15 +161,17 @@ export const DatePage = ({ date, lastfmUserId, song, siteDomain, twitterId }: Pr
               picture-in-picture="true"
               loading="lazy"
             ></iframe>
-            <div style={{display: 'flex', justifyContent: 'space-between', marginTop: '1rem'}}>
-            <a href={`/${yesterday}`}>
-              ←{yesterday}
-            </a>
-            <a href={`/${tomorrow}`}>
-              {tomorrow}→
-            </a>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                marginTop: "1rem",
+              }}
+            >
+              <a href={`/${yesterday}`}>←{yesterday}</a>
+              <a href={`/${tomorrow}`}>{tomorrow}→</a>
             </div>
-            <div style={{marginTop: "1rem"}}>
+            <div style={{ marginTop: "1rem" }}>
               <a href={`https://www.last.fm/user/${lastfmUserId}`}>
                 🎧 last.fm/@{lastfmUserId}
               </a>{" "}
@@ -111,7 +196,7 @@ export const Seo: React.FC<{
       <meta name="referrer" content="origin" />
       <meta name="viewport" content="width=device-width,initial-scale=1" />
       <meta charSet="UTF-8" />
-      <meta name="robots" content="noindex,nofollow,noarchive"/>
+      <meta name="robots" content="noindex,nofollow,noarchive" />
       <link
         rel="stylesheet"
         href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@500;700&family=Open+Sans:wght@600;700&display=swap"
