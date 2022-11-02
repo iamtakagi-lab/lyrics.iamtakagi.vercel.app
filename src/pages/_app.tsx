8859dccb509f9c5557676070351078b93ad6e25c
takagi <iam.takagi.dev@gmail.com>
2022-11-03 03:23:37 +0900

    clean: format

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,12 +3,6 @@ import "../styles/index.css";
 export const App: React.FC<{
   Component: React.FC;
   pageProps: any;
-}> = ({ Component, pageProps }) => {
-  return (
-    <>
-      <Component {...pageProps} />
-    </>
-  );
-};
+}> = ({ Component, pageProps }) => <Component {...pageProps} />;
 
 export default App;
